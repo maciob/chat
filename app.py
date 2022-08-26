0c233fca0f8a631b16407dc2c6e9fe75ccd6394e
Maciej <bekasmaciej@gmail.com>
2022-08-26 16:22:12 +0200

    First working version

---
+++ app.py
@@ -0,0 +1,42 @@
+from locale import format_string
+from logging import root
+from flask import Flask,render_template, request
+from datetime import datetime
+from flaskext.mysql import MySQL
+
+
+app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'app'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'pass'
+app.config['MYSQL_DATABASE_DB'] = 'mydb'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
+mysql.init_app(app)
+
+@app.route("/<int:room_id>",methods=["GET","POST"])
+def home(room_id): 
+	return render_template("index.html")
+
+@app.route("/api/chat/<int:room_id>",methods=["GET","POST"])
+def send(room_id):
+	conn = mysql.connect()
+	cur = conn.cursor()
+	row = ""
+	if request.method == "GET":
+		cur.execute(f"SELECT * FROM chat WHERE ID={room_id}")
+		data = cur.fetchall()
+		response = []
+		if data:
+			for i in data:
+				response.append(f'({i[1]}): {i[2]}')
+			return "\n".join(response)
+		else:
+			return "EMPTY HISTORY"
+	if request.method == "POST":
+		message = request.form.get("msg")
+		username = request.form.get("username")
+		cur.execute(f"INSERT INTO chat (ID,username,message) VALUES ('{room_id}','{username}','{message}');")	
+		conn.commit()	
+		return f'{message}'
+if __name__ == "__main__":
+	app.run()
