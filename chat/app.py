98043830733bfba46bd08af1492e35fd0dd5705d
Maciej <bekasmaciej@gmail.com>
2022-08-27 20:34:26 +0200

    Dockerised version

--- chat/app.py
+++ chat/app.py
@@ -3,14 +3,14 @@ from logging import root
 from flask import Flask,render_template, request
 from datetime import datetime
 from flaskext.mysql import MySQL
-
+from datetime import datetime
 
 app = Flask(__name__)
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'app'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'pass'
 app.config['MYSQL_DATABASE_DB'] = 'mydb'
-app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.3'
 mysql.init_app(app)
 
 @app.route("/<int:room_id>",methods=["GET","POST"])
@@ -28,14 +28,15 @@ def send(room_id):
 		response = []
 		if data:
 			for i in data:
-				response.append(f'({i[1]}): {i[2]}')
+				response.append(f'({i[3]} {i[1]}): {i[2]}')
 			return "\n".join(response)
 		else:
 			return "EMPTY HISTORY"
 	if request.method == "POST":
 		message = request.form.get("msg")
 		username = request.form.get("username")
-		cur.execute(f"INSERT INTO chat (ID,username,message) VALUES ('{room_id}','{username}','{message}');")	
+		date = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+		cur.execute(f"INSERT INTO chat (ID,username,message,date) VALUES ('{room_id}','{username}','{message}','{date}');")
 		conn.commit()	
 		return f'{message}'
 if __name__ == "__main__":
