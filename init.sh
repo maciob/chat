2351f9a33e0972b38f2fcf7e9f408cd55e39ef30
Maciej <bekasmaciej@gmail.com>
2023-03-08 11:25:19 +0100

    time change

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ docker rm -f $(docker ps -aq)
 docker build . -t chat:1.0 -f chat/Dockerfile
 docker run -d -p 8000:5000 chat:1.0
 docker run -d --name my-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=pass mysql:8.0.30
-sleep 602
+sleep 603
 docker exec my-mysql mysql -u root -ppass -e "CREATE USER 'app'@'172.17.0.2' IDENTIFIED WITH mysql_native_password BY 'pass';"
 docker exec my-mysql mysql -u root -ppass -e "CREATE DATABASE mydb;"
 docker exec my-mysql mysql -u root -ppass -e "GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'app'@'172.17.0.2' WITH GRANT OPTION;"
